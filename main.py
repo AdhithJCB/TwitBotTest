e79d0f0e2631d079942eb8fd1b8011f6ea8a224e
Adhith Jacob <55325514+AdhithJCB@users.noreply.github.com>
2022-01-19 01:26:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = "m63opE9m9xAMm0vA1mWDNnsGu"
-consumer_secret = "EoRZGFnlIXtVqbG6kk3c7PzmseJV7IILYq0Uyopq5BevSXkdQ1"
-key = "1457353798452645899-XIwG6gfrRp6pRqAMpISBKeEVuC7nEC"
-secret = "8RFGXmeRtSLRdBrtDaMExofsPQ2tJqCjOXvHntt4KQKeD"
+consumer_key = "CONSUMER KEY"
+consumer_secret = "SECRET"
+key = "KEY"
+secret = "SECRET"
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
